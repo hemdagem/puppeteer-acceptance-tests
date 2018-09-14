83fc94f3b53f7f705f13c6e8302430ebfdb33220
Hemang Rajyaguru <hemdagem@gmail.com>
2018-09-14 22:20:22 +0100

    simple test

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const puppeteer = require('puppeteer');
 
-// (async () => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto('https://www.arsenalhistory.co.uk');
-//   await page.screenshot({path: 'example.png'});
-
-//   await browser.close();
-// })();
-
 describe('Google', () => {
     beforeAll(async () => {
-      await page.goto('https://google.com');
+      await page.goto('https://www.arsenalhistory.co.uk');
     })
   
-    it('should display "google" text on page', async () => {
-      await expect(page).toMatch('google');
-      await page.screenshot({path: 'example.png'});
+    it('should display "arsenal" text on page', async () => {
+        await page.screenshot({path: 'example.png'});
+        await expect(page).toMatch('Arsenal');
     })
   });

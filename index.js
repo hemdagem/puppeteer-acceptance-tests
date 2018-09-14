6cd774d204c28cb41374b680cdac35f473935c0d
Hemang Rajyaguru <hemdagem@gmail.com>
2018-09-14 22:29:53 +0100

    minor tweaks

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const puppeteer = require('puppeteer');
 
-describe('Google', () => {
+describe('Arsenal History', () => {
     beforeAll(async () => {
       await page.goto('https://www.arsenalhistory.co.uk');
-    })
+    });
   
-    it('should display "arsenal" text on page', async () => {
-        await page.screenshot({path: 'example.png'});
+    it('should display "Arsenal" text on page', async () => {
+        await page.screenshot({path: 'arsenal.png'});
         await expect(page).toMatch('Arsenal');
-    })
+    });
   });
